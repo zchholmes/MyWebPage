04a02615b5349f2bb5e8ed597552651d1b0b83df
Chenhua Zhu <zchholmes@gmail.com>
2017-04-20 18:24:25 -0700

    php for curl

---
+++ Template/StoreInfo.php
@@ -0,0 +1,8 @@
+<?php
+print("{ 
+	\"StoreName\" : \"ZCHHolmes Education\",
+	\"StoreID\" : 1,
+	\"StoreLogo\" : \"http://cmpe272.zchholmes.cc/Pic/large/store_logo.png\",
+	\"StoreBanner\" : \"http://cmpe272.zchholmes.cc/Pic/large/store_banner.jpg\"
+	 }");
+?>
